f47f4f1eaff60eaf89d9b76c342e121fc7d75384
matthew <matthewbeaulieu10@gmail.com>
2021-10-18 19:27:13 -0500

    could've really screwed up index.json, pasted my code into it

--- index.js
+++ index.js
@@ -1,33 +1,36 @@
-var http = require('http');
-var url = require('url');
+const express = require('express')
+const expressHandlebars = require('express-handlebars')
+
+const app = express()
 
 
-const server = http.createServer((request, response) => {
-    // Write the request to the log. 
-    console.log(request);
 
-    // Header.
-    response.writeHead(200, {'Content-Type': 'text/html'});
-    response.write('<h3>Matthew Beaulieu Assignment Portfolio</h3>')
+// configure Handlebars view engine
+app.engine('handlebars', expressHandlebars({
+  defaultLayout: 'main',
+}))
+app.set('view engine', 'handlebars')
 
-    //Sprint 1
-    response.write("Sprint 1:<br>");
-    response.write("- <a href='https://calm-pond-031de9810.azurestaticapps.net/'>Getting to Know Me </a><br><br>");
+app.use(express.static(__dirname + '/public'))
 
-    //Sprint 2 
-    response.write("Sprint 2:<br>");
-    response.write("- <a href='https://wonderful-hill-09b0ee910.azurestaticapps.net'>Resume</a><br>");
-    response.write("- <a href='https://happy-dune-0996a0c10.azurestaticapps.net'>RegExp</a><br>");
-    response.write("- <a href='https://orange-dune-04a121b10.azurestaticapps.net'>Dice Roller</a><br><br>");
+const port = process.env.PORT || 3000
 
-    //Sprint 3 
-    response.write("Sprint 3:<br>")
+app.get('/', (req, res) => res.render('home'))
 
-    // Close the response
-    response.end('<br><h3>Thats it.</h3>');
-});
+app.get('/dr', (req,res) => res.render('DiceRoller'))
 
-const port = process.env.PORT || 1337;
-server.listen(port);
+app.get('/gtke', (req,res) => res.render('GettingToKnowEachOther'))
 
-console.log("Server running at http://localhost:%d", port);
+app.get('/resume', (req,res) => res.render('Resume'))
+
+app.get('/regexp', (req,res) => res.render('RegExp'))
+
+// custom 404 page
+app.use((req, res) => {
+  res.status(404)
+  res.render('404')
+})
+
+app.listen(port, () => console.log(
+  `Express started on http://localhost:${port}; ` +
+  `press Ctrl-C to terminate.`))
